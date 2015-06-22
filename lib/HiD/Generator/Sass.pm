e5facb61cc1700bee55dca328ff7873c18c45d2f
Jake Goldsborough <rjgoldsborough@gmail.com>
2015-06-22 12:43:59 -0400

    recommitting file as first letter capped only

---
+++ lib/HiD/Generator/Sass.pm
@@ -0,0 +1,76 @@
+package HiD::Generator::Sass;
+
+# ABSTRACT: transform SASS files to CSS
+
+=head1 DESCRIPTION
+
+=cut
+
+use Moose;
+with 'HiD::Generator';
+use File::Find::Rule;
+use Path::Tiny;
+use CSS::Sass;
+
+use 5.014;
+
+use HiD::VirtualPage;
+
+=method generate
+
+=cut
+
+has sass_sources => (
+  is      => 'ro',
+  isa     => 'Str',
+  default => sub {
+    my($self, $site) = @_;
+
+    $site->config->{sass}{sass_sources} ? 
+    $site->config->{sass}{sass_sources} :
+    '/_sass/screen.scss' 
+  }
+);
+
+has sass_output => (
+  is      => 'ro',
+  isa     => 'Str',
+  default => sub {
+    my($self, $site) = @_;
+
+    $site->config->{sass}{sass_output} ? 
+    $site->config->{sass}{sass_output} :
+    '/css'
+  }
+);
+
+sub generate {
+  my($self, $site) = @_;
+
+  my @sass_sources = @{ $site->config->{sass}{sass_sources} };
+
+  my $sass = CSS::Sass->new(
+    output_style  => SASS_STYLE_COMPRESSED
+  );
+
+  foreach my $file (@sass_sources) {
+    $site->INFO("* Compiling sass file - " . $file);
+    my $css = $sass->compile_file($file);
+    my $filename = path($file)->basename('.scss');
+
+    my $css_file = HiD::VirtualPage->new({
+      content => $css,
+      output_filename => $site->destination .
+                         $site->config->{sass}{sass_output} .
+                         $filename .
+                         '.css'
+    });
+
+    $site->add_object($css_file);
+  }
+
+  $site->INFO("* Compiled sass files successfully!");
+}
+
+__PACKAGE__->meta->make_immutable;
+1;
